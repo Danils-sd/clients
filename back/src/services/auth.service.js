63bd0a325328be1138a3603e78ef048183c0fc3f
Danils-sd <hlebushek.sd@gmail.com>
2024-04-12 20:11:03 +0300

    singIn

---
+++ back/src/services/auth.service.js
@@ -0,0 +1,31 @@
+const data = require("../configs/admin.config");
+
+async function singIn(login, password){
+    try {
+        const docRef = await data.collection("users").where("login", "==", login).get();
+        
+        if(docRef._size == 0){
+            return 401;
+        } else {
+            const users = [];
+            docRef.forEach(e => {
+                users.push({
+                    uid: e.id,
+                    login: e.data().login,
+                    password: e.data().password
+                })
+            })
+            if(users[0].password == password){
+                return users[0].uid;
+            }
+            return 402;
+        }
+    } catch (error) {
+        console.log(error);
+        return 400;
+    }
+}
+
+module.exports = {
+    singIn
+}
